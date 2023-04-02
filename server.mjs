058394909aa5ca01fa80d6460ae686fb56db57e5
subham-dhakal <subhamdhakal123@gmail.com>
2023-04-02 19:29:37 +0545

    express graphql server setup

--- server.mjs
+++ server.mjs
@@ -1,20 +1,54 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 import './model/associations.js';
 
 import sequelize from './sequelize.js';
 
 import  resolvers  from './graphql/resolvers.js';
 import  typeDefs  from './graphql/schema.js';
+import { log } from 'console';
 
 
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+// });
+
+// const { url } = await startStandaloneServer(server);
+// console.log(` Server ready at ${url}`);
+
+
+// Apollo Server's built-in Express middleware
+const app = express();
+const httpServer = http.createServer(app);
+
+// Set up Apollo Server
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+});
+await server.start();
+
+app.get("/", (_, res) => {
+res.send("Hello API");
 });
 
-const { url } = await startStandaloneServer(server);
-console.log(` Server ready at ${url}`);
+app.use(
+  '/graphql',
+  cors(),
+  bodyParser.json(),
+  expressMiddleware(server),
+);
+
+await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+console.log(`🚀 Server ready at http://localhost:4000/graphql`);
 
 sequelize.authenticate().then(() => {
     console.log(`Database Connected`);
